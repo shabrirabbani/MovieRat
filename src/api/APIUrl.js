461b61f388d4466b1c8fd551fe14c03fab08e570
Muhammad Shabri rabbani <66266925+shabrirabbani@users.noreply.github.com>
2024-07-09 16:22:50 +0700

    first commit

--- src/api/APIUrl.js
+++
@@ -1,5 +0,0 @@
-const URL = "https://api.themoviedb.org/3/movie/now_playing?api_key";
-const API_KEY = "f90f2cf35e079e494e6b06353f7b96fd"
-const API_URL = `${URL}=${API_KEY}`
-
-export default API_URL
